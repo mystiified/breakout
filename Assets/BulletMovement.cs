5cca0b9b018b37f57d987a4af64b5c1b1a6e34b2
mystiified <152400721+mystiified@users.noreply.github.com>
2023-12-15 16:46:24 +0100

    Small edit 1

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -12,17 +12,19 @@ public class BulletMovement : MonoBehaviour
     {
         BulletRigidBody = GetComponent<Rigidbody>();
         BulletRigidBody.AddForce(new Vector3(Random.Range(-15.0f, 15.0f), 15, 0));
+
+        float speed = 5;
+        BulletRigidBody.velocity = BulletRigidBody.velocity.normalized * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed = 15;
-        BulletRigidBody.velocity = BulletRigidBody.velocity.normalized * speed;
 
         if (transform.position.y < -0.1346011)
         {
             transform.position = new Vector3(0, 2, 0);
+            BulletRigidBody.velocity = Vector3.zero;
             BulletRigidBody.AddForce(new Vector3(Random.Range(-15.0f, 15.0f), 15, 0));
         }
     }
